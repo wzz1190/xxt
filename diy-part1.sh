77e81821d46d94fb6a3c5a7b5852f52434cd521e
wzz1190 <79913593+wzz1190@users.noreply.github.com>
2021-08-23 01:34:25 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,9 +16,9 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo "src/gz kuoruan_packages http://openwrt.kuoruan.net/packages/releases/$(. /etc/openwrt_release ; echo $DISTRIB_ARCH)" \
+#echo "src/gz kuoruan_packages http://openwrt.kuoruan.net/packages/releases/$(. /etc/openwrt_release ; echo $DISTRIB_ARCH)" \
   >> /etc/opkg/customfeeds.conf
 
 
-git clone https://github.com/wzz1190/luci-app-webshell.git package/luci-app-webshell
+#git clone https://github.com/wzz1190/luci-app-webshell.git package/luci-app-webshell
 
